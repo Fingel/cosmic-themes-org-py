a02244bd3585ec7a80814c9962051d0023d97b46
Austin Riba <austin@m51.io>
2024-08-25 21:19:47 -0700

    wrong docker compose

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,4 +3,4 @@ echo "Deploying cosmic-themes, prepare for liff off..."
 cd /home/austin/docker/cosmic-themes-py/build
 docker build -t cosmic-themes-py .
 cd ../
-docker-compose up -d
+docker compose up -d
